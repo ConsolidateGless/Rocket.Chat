a64c1d9658aa56480bfef52fef28b3db0d201231
pstieger92 <patrick@freilichopenarts.com>
2019-11-21 10:39:57 +0100

    added color styling for cons link

--- app/autolinker/client-custom/consLinkAutoLinker.js
+++ app/autolinker/client-custom/consLinkAutoLinker.js
@@ -2,7 +2,7 @@ class ConsLinkAutoLinker {
 	static link(message) {
 		const consRegexPattern = /cons\.+([\d]+)/gim;
 		message.html = message.html.replace(consRegexPattern, (match, aktId) =>
-			`<a href="consxml:editakt?${ aktId }">${ match.trim() }</a>`
+			`<a class="conslink" href="consxml:editakt?${ aktId }">${ match.trim() }</a>`
 		);
 	}
 }
